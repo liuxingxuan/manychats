d79597c29c89a95405f4b891b292c5fc22b36177
lxx <853555374@qq.com>
2019-03-28 23:46:31 +0800

    nettychat

---
+++ nettychat/src/main/java/io/lxx/server/ServerHandler2.java
@@ -0,0 +1,12 @@
+package io.lxx.server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+public class ServerHandler2 extends ChannelInboundHandlerAdapter  {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("ServerHandler2 receive msg:"+msg.toString());
+        ctx.channel().writeAndFlush("this is ServerHandler2 reply msg happend at !"+System.currentTimeMillis());
+    }
+}
